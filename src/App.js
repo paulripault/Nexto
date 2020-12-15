446782b0aa925cbac1996827960872aaa543e59b
paul ripault <paulripault0@gmail.com>
2020-12-15 11:10:52 +0100

    commit initial

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Navbar from './Navbar/navbar';
+import Section_index from './Section_index/section_connexion';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Section_index />
+
+    </div >
+  );
+}
+
+export default App;
