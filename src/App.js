46fa67d7481969298f3ff7d9bf083c4e27a70bf5
paul ripault <paulripault0@gmail.com>
2021-01-04 13:53:46 +0100

    MasterCommit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link, BrowserRouter } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Features from './pages/Features';
+import Contact from './pages/Contact';
+import About from './pages/About';
 import './App.css';
-import Navbar from './Navbar/navbar';
-import Section_index from './Section_index/section_connexion';
+
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Section_index />
-      <p>Test</p>
-    </div >
+    <BrowserRouter>
+      <div className="App">
+        <Router>
+          <div className="container">
+            <Navbar />
+          </div>
+
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/features' component={Features} />
+            <Route exact path='/contact' component={Contact} />
+            <Route path='/about' component={About} />
+          </Switch>
+        </Router>
+      </div >
+    </BrowserRouter>
   );
 }
 
