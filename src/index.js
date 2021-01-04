8b47f4b21d31da057d3d5e5bfcf24396ef6fb4f9
Alex Lri <alexandre.lairie@gmail.com>
2021-01-04 09:57:11 +0100

    test

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './fonts/Roboto-Regular.ttf';
+import './fonts/Helvetica.ttf';
+import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 
