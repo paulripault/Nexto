2ec9149c0b6be727751c70ea4b2b9ecab198f7d9
paul ripault <paulripault0@gmail.com>
2021-01-05 15:52:33 +0100

    fin

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './Navbar.css';
-
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import logo from '../img/logo-example1.jpg';
-
-export default function navbar() {
-    return (
-        <div className="container" id="Navbar">
-            <Navbar bg="transparent" expand="lg">
-                <Navbar.Brand href="/" className="logo-title">
-                    <img src={logo} width="100" roundedCircle></img></Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ml-auto nav-links">
-                        <Nav.Link href="/" className="hvr-underline-from-left">Accueil</Nav.Link>
-                        <Nav.Link href="/features" className="hvr-underline-from-left">Fonctionnalités</Nav.Link>
-                        <Nav.Link href="/contact" className="hvr-underline-from-left">Contact</Nav.Link>
-                        <Nav.Link href="/about" className="hvr-underline-from-left">A propos</Nav.Link>
-                    </Nav>
-
-                </Navbar.Collapse>
-            </Navbar>
-        </div >
-
-
-
-    )
-}
-=======
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Navbar.css";
@@ -67,4 +34,3 @@ export default function navbar() {
     </div>
   );
 }
->>>>>>> alex
